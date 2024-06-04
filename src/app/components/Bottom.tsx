55e4f73ce7a77e5bf66a6446672d9a343a65bebd
zkcairo <167991601+zkcairo@users.noreply.github.com>
2024-06-04 14:52:42 +0200

    Add files via upload

--- src/app/components/Bottom.tsx
+++ src/app/components/Bottom.tsx
@@ -5,8 +5,8 @@ const Bottom = () => {
   return (
     <div className="mb-16 text-center">
     <button>Discord (soon)</button>
-    <button>Twitter (soon)</button>
-    <button>Docs (soon)</button>
+    <button><a href="https://twitter.com/0liqlend" target="_blank">Twitter</a></button>
+    <button><a href="https://docs.0liqlend.com">Docs</a></button>
     <button>Faq (soon)</button>
     <button><a href="https://github.com/zkcairo" target="_blank">Github</a></button>
   </div>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -110,6 +110,9 @@ const Header = () => {
         <meta name="twitter:image" content="https://0LiqLend.com/image.png" />
         <meta name="twitter:url" content="https://0LiqLend.com" />
 
+        <link rel="icon" href="https://0LiqLend.com/favicon.jpeg" />
+
+
       <header
         ref={dropdownRef}
         className="w-full fixed backdrop-blur-2xl dark:border-neutral-800 lg:bg-gray-200 lg:dark:bg-zinc-800/50 left-0 top-0  z-10 flex flex-wrap gap-4 py-2 px-4 md:py-4 md:px-10  justify-between items-center"
